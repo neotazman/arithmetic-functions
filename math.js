06917ba0ce031c41f4646b1d5c6c661e4971d097
Tristan Alie <TristanAlie@yahoo.com>
2021-08-05 16:38:23 -0400

    finished the power operator

--- math.js
+++ math.js
@@ -18,9 +18,19 @@ const multiply = (x, y) => {
         return null
     }
     let result = 0
-    for (let i = 0; i < y; i++) {
+    for (let i = 0; i < y; i = add(i, 1)) {
         result = add(result, x)
     }
     return result
 }
 
+const power = (x, n) => {
+    if(!checkIfNumber(n) || n < 0) { //x has to be a natural number
+        return null
+    }
+    let result = 1
+    for(let i = 0; i < n; i = add(i, 1)) {
+        result = multiply(result, x)
+    }
+    return result
+}
