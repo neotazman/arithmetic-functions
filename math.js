d91ebb84e657d74d239063776d71657a19c175b3
Tristan Alie <TristanAlie@yahoo.com>
2021-08-05 16:18:23 -0400

    added a check if it's a number

--- math.js
+++ math.js
@@ -1,7 +1,22 @@
 // Your Code Here.
-const add = (x, y) => x + y
+
+const checkIfNumber = (x) => { //x should be a number
+    if(typeof x !== 'number'){
+        return false;
+    } else return true
+}
+
+const add = (x, y) => {
+    if( !checkIfNumber(x) || !checkIfNumber(y)) {
+        return null
+    }
+    return x + y
+} 
 
 const multiply = (x, y) => {
+    if(!checkIfNumber(y)) {
+        return null
+    }
     let result = 0
     for (let i = 0; i < y; i++) {
         result = add(result, x)
